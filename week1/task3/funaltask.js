2ece3519739bb5efcd34ada4e18717a9e1781bee
adlid <baidildaevbahon08@gmail.com>
2021-04-27 14:24:44 +0600

    End Task

--- week1/task3/funaltask.js
+++ week1/task3/funaltask.js
@@ -3,6 +3,7 @@
 // как runningSum[i] = sum(nums[0]…nums[i]).
 // Вернуть сумму элементов массива nums.
 let emp = [];
+let arr = [1, 2, 3, 4, 5];
 
 function runningSum(nums) {
     let count = 0;
@@ -10,7 +11,6 @@ function runningSum(nums) {
         emp.push(count += nums[i])
     }
 }
-let arr = [1, 2, 3, 4, 5];
 runningSum(arr);
 console.log(emp)
     // with method array
@@ -45,3 +45,18 @@ for (let i = 0; i < strinarr.length; i++) {
 }
 emtyArr = emtyArr.join('');
 console.log(emtyArr)
+
+// Задание №4
+// Предложение — это список слов, разделенных одним пробелом без начальных и
+// конечных пробелов. Каждое из слов состоит только из прописных и строчных английских
+// букв (без знаков препинания).Например, &quot;Hello World&quot;, &quot;HELLO&quot; и
+// &quot;hello world hello world&quot; — это все предложения.Вам дается предложение s и
+// целое число k. Вы хотите усечь s так, чтобы он содержалтолько первые k слов. Предложение
+// s возвращается после его усечения.
+
+function truncateSentence(s, k) {
+    result = s.split(' ').map(elem => elem)
+    result.splice(k)
+    console.log(result)
+}
+truncateSentence('What is the solution to this problem', 2)
